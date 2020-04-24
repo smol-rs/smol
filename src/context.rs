04081d7cab663b4836cfb19e456c933ee1241a8d
Taiki Endo <te316e89@gmail.com>
2020-04-24 21:56:49 +0900

    Rename tokio feature to tokio02

--- src/context.rs
+++ src/context.rs
@@ -4,10 +4,10 @@
 
 /// Enters the tokio context if the `tokio` feature is enabled.
 pub(crate) fn enter<T>(f: impl FnOnce() -> T) -> T {
-    #[cfg(not(feature = "tokio"))]
+    #[cfg(not(feature = "tokio02"))]
     return f();
 
-    #[cfg(feature = "tokio")]
+    #[cfg(feature = "tokio02")]
     {
         use once_cell::sync::Lazy;
         use tokio::runtime::Runtime;
